72808951dd9637528a65adaf3eb3b7fbcfd5b906
Isus Ramzy <isusramzy@proton.me>
2025-04-05 20:17:07 +0200

    Fix problem + Migrate to gopher-lua (3rd migration, 4 in total)

--- main.go
+++ main.go
@@ -5,12 +5,13 @@ import (
 	"fmt"
 	"os"
 
-	lua "github.com/Shopify/go-lua"
 	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
+	lua "github.com/yuin/gopher-lua"
+	luar "layeh.com/gopher-luar"
 )
 
-var state *lua.State = lua.NewState()
+var state *lua.LState = lua.NewState()
 var thedocument Document = Document{}
 var themodel model = model{}
 
@@ -83,13 +84,12 @@ func callAllSpecialCallables() {
 	for _, element := range thedocument.Elements {
 		if element.Class == "input" && isFocused(element.Id) {
 			if element.Callable != "" {
-				state.Global(element.Callable)
-				state.PushGoFunction(func(p *lua.State) int {
-					err, _ := p.ToInteger(1)
-					log(string(err))
-					return 0
-				})
-				state.ProtectedCall(0, 0, 1)
+				whatever := state.GetGlobal(element.Callable)
+				state.Push(whatever)
+				state.CallByParam(lua.P{
+					Fn:   state.GetGlobal(element.Callable),
+					NRet: 0,
+				}, nil)
 				updateTextSpecialElements()
 			}
 		}
@@ -133,8 +133,8 @@ func (m model) View() string {
 	return text
 }
 
-func Print(p *lua.State) int {
-	mystr, _ := p.ToString(1)
+func Print(p *lua.LState) int {
+	mystr := p.ToString(1)
 	fmt.Println("\n\n" + mystr)
 	return 0
 }
@@ -160,36 +160,36 @@ func createInitialSpecialElements() {
 	}
 }
 
-func new_element(p *lua.State) int {
-	class, _ := p.ToString(1)
-	text, _ := p.ToString(2)
-	id, _ := p.ToString(3)
-	callable, _ := p.ToString(4)
-	placeholder, _ := p.ToString(5)
-	p.PushUserData(Element{class, text, id, callable, placeholder})
+func new_element(p *lua.LState) int {
+	class := p.ToString(1)
+	text := p.ToString(2)
+	id := p.ToString(3)
+	callable := p.ToString(4)
+	placeholder := p.ToString(5)
+	p.Push(luar.New(p, Element{class, text, id, callable, placeholder}))
 	return 1
 }
 
-func add_element(p *lua.State) int {
-	myInterfacedElement := p.ToUserData(1)
-	myelement := myInterfacedElement.(Element)
-	thedocument.Elements = append(thedocument.Elements, myelement)
+func add_element(p *lua.LState) int {
+	myElement := p.ToUserData(1).Value
+	//myelement := myInterfacedElement.(Element)
+	thedocument.Elements = append(thedocument.Elements, myElement.(Element))
 	return 0
 }
 
-func get_by_Id(p *lua.State) int {
-	Id, _ := p.ToString(1)
+func get_by_Id(p *lua.LState) int {
+	Id := p.ToString(1)
 	for _, element := range thedocument.Elements {
 		if element.Id == Id {
-			p.PushUserData(element)
+			p.Push(luar.New(p, element))
 		}
 	}
 	return 1
 }
 
-func set_by_Id(p *lua.State) int {
-	Id, _ := p.ToString(1)
-	AAAA := p.ToUserData(2)
+func set_by_Id(p *lua.LState) int {
+	Id := p.ToString(1)
+	AAAA := p.ToUserData(2).Value
 	newElement := AAAA.(Element)
 	for i, element := range thedocument.Elements {
 		if element.Id == Id {
@@ -199,35 +199,14 @@ func set_by_Id(p *lua.State) int {
 	return 0
 }
 
-func param_of_element(p *lua.State) int {
-	param, _ := p.ToString(1)
-	myElement := p.ToUserData(2)
-	element := myElement.(Element)
-
-	var toPush string
-	if param == "text" {
-		toPush = element.Text
-	} else if param == "class" {
-		toPush = element.Class
-	} else if param == "callable" {
-		toPush = element.Callable
-	} else if param == "placeholder" {
-		toPush = element.Placeholder
-	} else {
-		toPush = "No Param Found"
-	}
-	p.PushString(toPush)
-	return 1
-}
-
 func log(text string) {
 	file, _ := os.OpenFile("log.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	defer file.Close()
 	file.WriteString(text + "\n")
 }
 
-func lua_log(p *lua.State) int {
-	text, _ := p.ToString(-1)
+func lua_log(p *lua.LState) int {
+	text := p.ToString(0)
 	log(text)
 	return 0
 }
@@ -239,15 +218,17 @@ func main() {
 <elements>
   <element class="text">Hello from XML! Enter your name:</element>
   <element class="input" id="name" callable="name_handler" placeholder="John Doe"></element>
-  <element class="text" id="myid">Test</element>
+  <element class="text" id="myid"></element>
+  <element class="text" id="thiswork"></element>
 </elements>
 <script>
 add_element(new_element("text", "Hello, from Lua!"))
 --add_element(new_element("input", "", "randomID", "callable1", "Ok"))
 function name_handler()
-    local text = param_of_element("text", get_by_Id("name"))
-    local newElem = new_element("text", text) 
-    set_by_Id("myid", newElem)
+    local text = "Hello "..get_by_Id("name").Text .. "!"
+    --print(text)
+    set_by_Id("myid", new_element("text", text, "myid"))
+    set_by_Id("thiswork", new_element("text", "WORKS!"))
 end
 </script>
   </document>
@@ -257,14 +238,14 @@ end
 		fmt.Println(err)
 		os.Exit(1)
 	}
-	lua.OpenLibraries(state)
+	state.OpenLibs()
+	defer state.Close()
 	state.Register("log", lua_log)
 	state.Register("add_element", add_element)
 	state.Register("new_element", new_element)
 	state.Register("get_by_Id", get_by_Id)
 	state.Register("set_by_Id", set_by_Id)
-	state.Register("param_of_element", param_of_element)
-	err = lua.DoString(state, thedocument.Script)
+	err = state.DoString(thedocument.Script)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
